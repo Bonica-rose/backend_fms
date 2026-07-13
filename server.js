72a41af52e16b236a36f5a6b57f20ed3ee91149a
Bonica-rose <eveningstarforever@gmail.com>
2026-07-13 21:51:03 +0530

    origin all

--- server.js
+++ server.js
@@ -13,10 +13,7 @@ const authRoutes = require("./src/routes/authRoutes");
 const app = express()
 
 app.use(cors({
-    origin:[
-        process.env.CLIENT_URL,
-        'http://localhost:5173'
-    ],
+    origin:'*',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true
